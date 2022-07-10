5befb6f817ef0e0fc1e7edda4bd257f833a8e64d
Nimish Pandey <pandey.nimish11@gmail.com>
2022-07-10 12:43:14 +0530

    Update Merge_K_Sorted_lists.java

--- Merge_K_Sorted_lists.java
+++ Merge_K_Sorted_lists.java
@@ -1,3 +1,6 @@
+/*This approach takes 2 linked lists at a time, merges them and them again merges it with the thirf iteratively
+
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -37,3 +40,50 @@ class Solution {
     }
         
     }
+//---------------------------------------------------------------------------------------------------------------------------------------------//
+
+//ANOTHER APPROACH
+
+//compare elements one by one and keep on adding the minimun in the linked list.
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        ArrayList<Integer> list = new ArrayList();
+        
+        for(ListNode ln : lists)
+        {
+            while(ln!=null)
+            {
+                list.add(ln.val);
+                ln = ln.next;
+            }
+            
+        }
+        
+        Collections.sort(list);
+        
+        ListNode node = new ListNode(0);
+        ListNode head = node;
+        
+        for(int i : list)
+        {
+            ListNode t = new ListNode(i);
+            node.next = t;
+            node = node.next;
+        }
+        node.next = null;
+        return head.next;
+        
+    }
+}
